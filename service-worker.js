7a070d0e6827c121c8741b7506b79b60dc3d2e30
Abhinav Kaar <abhinav.kaar.ds29@heritageit.edu.in>
2026-01-28 12:16:20 +0530

    Simplify service worker by removing cache handling
    Removed caching logic and simplified event listeners.

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,2 @@
-const CACHE = "weather-v1";
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js"
-      ])
-    )
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
+self.addEventListener("install",()=>self.skipWaiting());
+self.addEventListener("fetch",()=>{});
